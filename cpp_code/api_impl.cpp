9b87d8a920fc7e0b9e4f650ad28a2f45531d4007
Samuel van Gurp <your_email@example.com>
2025-10-29 12:01:34 +0100

    Refactor getHay function and implement constructImage for image processing

--- cpp_code/api_impl.cpp
+++ cpp_code/api_impl.cpp
@@ -1,5 +1,19 @@
+#include "api_impl.hpp"
+
 int hay = 42;
 
-extern "C" int getHay() {
-    return hay;
+int getHay() {
+    return testFunction();
+}
+
+void constructImage(const uint8_t *data, int width, int height, int stride)
+{
+    Image img(data, width, height, stride);
+    img.SaveAsPGM();
+    std::cout << "Image processed and saved as output.pgm\n";   
+}
+
+int testFunction()
+{
+    return 123;
 }
--- cpp_code/api_impl.hpp
+++ cpp_code/api_impl.hpp
@@ -1,10 +1,18 @@
 #pragma once
+
+#include <cstdint>
+#include "Image.hpp"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 int getHay(void);
 
+void constructImage(const uint8_t* data, int width, int height, int stride);
+
 #ifdef __cplusplus
 }
 #endif
+
+int testFunction();
