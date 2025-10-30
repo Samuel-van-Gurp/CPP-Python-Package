424718f1ee8ae20ead7af42d90a11c375bf65bec
Samuel van Gurp <your_email@example.com>
2025-10-30 16:10:08 +0100

    Refactor constructImage function: streamline image saving and contour overlay process

--- cpp_code/api_impl.cpp
+++ cpp_code/api_impl.cpp
@@ -1,4 +1,9 @@
 #include "api_impl.hpp"
+#include "DataObjects/Image.hpp"
+#include "DataObjects/Contour.hpp"
+#include "DataObjects/Point.hpp"
+#include "IO/WriteImage.hpp"
+#include "Algorithm/SnakeEngine.hpp"
 
 int hay = 42;
 
@@ -8,9 +13,17 @@ int getHay() {
 
 void constructImage(const uint8_t *data, int width, int height, int stride)
 {
+    WriteImage writer;
     Image img(data, width, height, stride);
-    img.SaveAsPGM();
-    std::cout << "Image processed and saved as output.pgm\n";   
+    writer.saveImage(img);
+    std::cout << "Image processed and saved as output.pgm\n";
+
+    Contour contour(img.GetWidth() / 3, img.GetCenter(), 50);
+    // writer.ContourOverLay(contour, img);
+    SnakeEngine engine(img, contour);
+    engine.RunSnake(100);
+    writer.ContourOverLay(contour, img);
+
 }
 
 int testFunction()
