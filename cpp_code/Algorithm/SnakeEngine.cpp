f221540d90220c96c664e1f6d101baea90e722ff
Samuel van Gurp <your_email@example.com>
2025-10-30 16:09:25 +0100

    inital algo

--- cpp_code/Algorithm/SnakeEngine.cpp
+++ cpp_code/Algorithm/SnakeEngine.cpp
@@ -1,10 +1,44 @@
 #include "SnakeEngine.hpp"
 
-SnakeEngine::SnakeEngine(const Image &image)
-    : m_image(image)
+SnakeEngine::SnakeEngine(const Image &image, Contour &contour)
+    : m_image(image), m_contour(contour)
 {
+
 }
 
-SnakeEngine::~SnakeEngine()
+void SnakeEngine::RunSnake(int iterations)
 {
+    // loop
+    for (int i = 0; i < iterations; ++i)
+    {
+        EvolveContour();
+    }
+
+}
+
+void SnakeEngine::EvolveContour()
+{
+    // loop over contour points
+    int contourSize = m_contour.Size();
+    for (int i = 0; i < contourSize; ++i)
+    {
+        // get current point
+        Point& p = m_contour[i];
+
+        // get next step based on image gradient at point location
+        Point nextStep = getNextStep(p);
+        m_contour[i] = nextStep;
+
+        // print initial and new point for debugging
+        std::cout << "Point " << i << ": (" << p.X << ", " << p.Y << ") -> ("
+                  << nextStep.X << ", " << nextStep.Y << ")\n";
+
+    }
+}
+
+Point SnakeEngine::getNextStep(Point& p)
+{
+    // get the highest value direction around point p (8 connectivity)
+    Point nextPoint = m_image.GetCoordinateOfHighestValueDirection(p);
+    return nextPoint;
 }
--- cpp_code/Algorithm/SnakeEngine.hpp
+++ cpp_code/Algorithm/SnakeEngine.hpp
@@ -1,10 +1,19 @@
-#include "Image.hpp"
+#include "DataObjects/Image.hpp"
+#include "DataObjects/Contour.hpp"
+#include "IO/WriteImage.hpp"
 
 class SnakeEngine
 {
-private:
-    const Image m_image;
-public:
-    SnakeEngine(const Image& image);
-    ~SnakeEngine();
+    public:
+        SnakeEngine(const Image& image, Contour& contour);
+
+        void RunSnake(int iterations); 
+
+        void EvolveContour();
+        Point getNextStep(Point &p);
+
+    private:
+        const Image& m_image;
+        Contour& m_contour;
+
 };
