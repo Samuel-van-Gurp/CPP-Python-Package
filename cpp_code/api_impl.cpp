400c5ab206929d1f112aa2a3fe8a53ae0c1c242f
Samuel van Gurp <your_email@example.com>
2025-11-05 09:48:19 +0100

    Refactor constructImage function: restore contour overlay and adjust SnakeEngine parameters

--- cpp_code/api_impl.cpp
+++ cpp_code/api_impl.cpp
@@ -15,13 +15,14 @@ void constructImage(const uint8_t *data, int width, int height, int stride)
 {
     WriteImage writer;
     Image img(data, width, height, stride);
+    img.PrepImage();
     writer.saveImage(img);
     std::cout << "Image processed and saved as output.pgm\n";
 
     Contour contour(img.GetWidth() / 3, img.GetCenter(), 50);
-    // writer.ContourOverLay(contour, img);
-    SnakeEngine engine(img, contour);
-    engine.RunSnake(100);
+    writer.ContourOverLay(contour, img);
+    SnakeEngine engine(img, contour, 0.5f, 0.5f);
+    engine.RunSnake(15);
     writer.ContourOverLay(contour, img);
 
 }
