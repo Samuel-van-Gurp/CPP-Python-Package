2dc5c5e6f93a3778eccbc618963beffbe92c98bf
Samuel van Gurp <your_email@example.com>
2026-01-22 14:11:20 +0100

    Add configuration file for storing paths and parameters

---
+++ python/Config.py
@@ -0,0 +1,11 @@
+# place to store configuration variables such as file paths
+
+import os
+from pathlib import Path
+
+# path to the C++ pybindings build directory
+BASE_DIR = Path(__file__).resolve().parent
+
+PYBINDINGS_DIR = BASE_DIR.parent / "build" / "Debug"
+
+PARAMETER_JSON_PATH = BASE_DIR / "Params" / "params.json"
