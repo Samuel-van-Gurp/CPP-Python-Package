cded96b77206952875e411695a92925b2145d77a
Samuel van Gurp <your_email@example.com>
2026-01-22 11:35:04 +0100

    Add ImageInfo struct and remove C linkage from Point struct, so i can ecpose them using pybind11 to python

---
+++ cpp_code/DataObjects/ImageInfo.hpp
@@ -0,0 +1,12 @@
+#ifndef DB5B6CD2_F18B_4D27_8E8B_17A7CC8CCFD7
+#define DB5B6CD2_F18B_4D27_8E8B_17A7CC8CCFD7
+
+#include <vector>
+
+struct ImageInfo {
+    std::vector<float> data;
+    uint16_t width;
+    uint16_t height;
+};
+
+#endif /* DB5B6CD2_F18B_4D27_8E8B_17A7CC8CCFD7 */
--- cpp_code/DataObjects/Point.hpp
+++ cpp_code/DataObjects/Point.hpp
@@ -3,18 +3,10 @@
 
 #include <cstdint>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 struct Point {
     float X;
     float Y;
 };
 
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // POINT_HPP 
