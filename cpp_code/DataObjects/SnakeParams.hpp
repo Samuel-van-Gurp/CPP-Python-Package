feb7a4861e57442485ff88402e8544e56f0b4637
Samuel van Gurp <your_email@example.com>
2026-01-22 11:35:35 +0100

    Move Snakeparams from python dataclass to the C++ side

---
+++ cpp_code/DataObjects/SnakeParams.hpp
@@ -0,0 +1,17 @@
+#ifndef E76829F6_A29E_4121_9FCB_EAEB5F351E88
+#define E76829F6_A29E_4121_9FCB_EAEB5F351E88
+
+// Define the SnakeParams struct
+struct SnakeParams {
+    double alpha;
+    double beta;
+    int iterations;
+    double contour_center_x;
+    double contour_center_y;
+    double contour_radius_x;
+    double contour_radius_y;
+    int contour_points;
+};
+
+
+#endif /* E76829F6_A29E_4121_9FCB_EAEB5F351E88 */
