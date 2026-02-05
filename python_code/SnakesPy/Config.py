73f8a0e6b9f0fa47fd6c0ef72d304aa82ac20860
Samuel van Gurp <your_email@example.com>
2026-02-05 11:35:11 +0100

    rename folder and package the code  (only works locally becaus paths to the .pyd are hardcoded)

---
+++ python_code/SnakesPy/Config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# path to the C++ pybindings build directory
+BASE_DIR = Path(__file__).resolve().parent
+
+PYBINDINGS_DIR = BASE_DIR.parent.parent / "build" / "Debug"
+
+PARAMETER_JSON_PATH = BASE_DIR.parent / "Params" / "params.json"
---
+++ python_code/SnakesPy/test_api.py
@@ -0,0 +1,10 @@
+from . import ActiveContour
+
+def main():
+
+    active_contour = ActiveContour("Images/astronaut.png")
+    active_contour.run()
+
+
+main()
+
