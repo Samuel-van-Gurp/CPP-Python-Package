015a7ebf58d7cf0e1e045b7bf8d825284f473088
Samuel van Gurp <your_email@example.com>
2026-02-09 11:46:58 +0100

    Refactor Config and Params classes to improve path handling and default parameter initialization

--- python_code/SnakesPy/Config.py
+++ python_code/SnakesPy/Config.py
@@ -1,8 +1,12 @@
 from pathlib import Path
+import os
 
 # path to the C++ pybindings build directory
 BASE_DIR = Path(__file__).resolve().parent
 
-PYBINDINGS_DIR = BASE_DIR.parent.parent / "build" / "Debug"
+PYBINDINGS_DIR = "C:\\Users\\svangurp\\Desktop\\CPP_PY_package\\python_code\\SnakesPy\\pybindings.cp314-win_amd64.pyd" # BASE_DIR.parent.parent / "build" / "Debug"
 
-PARAMETER_JSON_PATH = BASE_DIR.parent / "Params" / "params.json"
+# Look for params.json in the current working directory (where user's script is)
+import json
+
+PARAMETER_JSON_PATH = Path(os.getcwd()) / "params.json"
