a67008121bf15f8603fe882267b9b12fa5a57bfd
Samuel van Gurp <your_email@example.com>
2026-02-09 11:45:38 +0100

    Refactor package structure and add setup configuration for distribution (single platfrom)

--- python_code/SnakesPy/__init__.py
+++ python_code/SnakesPy/__init__.py
@@ -1,3 +1,3 @@
-from ActiveContour import ActiveContour
+from .ActiveContour import ActiveContour
 
 __version__ = "0.1.0"
---
+++ python_code/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages, Distribution
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
+
+
+setup(
+    name="SnakesPy",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"SnakesPy": ["pybindings*.pyd", "params.json"]},
+    distclass=BinaryDistribution,
+)
